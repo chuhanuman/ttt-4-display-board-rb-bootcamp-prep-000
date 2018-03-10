a63b1f8afa5c46026e219d35cc03b67d7ddb56f5
Hanuman Chu <chuhanuman@gmail.com>
2018-03-10 08:57:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  i=0;
-  while (i<10)
-    i=i+1
-    #HERE
   puts (" #{board[0]} | #{board[1]} | #{board[2]} ")
   puts ("-----------")
   puts (" #{board[3]} | #{board[4]} | #{board[5]} ")
